e04e6a74ab5ee97ee3b1af9c966dc174c4a464df
ejshi <1737778174@qq.com>
2018-01-26 00:21:08 +0800

    git init

---
+++ regulus-account-consumer/src/main/java/org/regulus/account/consumer/App.java
@@ -0,0 +1,13 @@
+package org.regulus.account.consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
